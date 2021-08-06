d639fc485aaeced40677c36f7d064e141defba24
Pat Anderson <patrick.b.anderson@gmail.com>
2021-08-06 12:26:34 -0500

    able to wipe inputs with local state in MakeInquiry.jsx

--- src/components/MakeInquiry/MakeInquiry.jsx
+++ src/components/MakeInquiry/MakeInquiry.jsx
@@ -26,7 +26,8 @@ const MakeInquiry = () => {
 
     const addInquiry = ( object ) =>{
         console.log( 'in addInquiry w/:', object );
-        dispatch( { type: 'SEND_INQUIRY', payload: object } )
+        dispatch( { type: 'SEND_INQUIRY', payload: object } );
+        setTempInquiry({ ...tempInquiry, first_name: '', last_name: '', email: '', phone: '', description: ''});
     }
 
     return (
@@ -34,11 +35,11 @@ const MakeInquiry = () => {
             <br />
             <br />
             <h2>Make an inquiry</h2>
-            <input type="text" placeholder="first name" onChange={handleFirstName} />
-            <input type="text" placeholder="second name" onChange={handleLastName} />
-            <input type="text" placeholder="email" onChange={handleEmail} />
-            <input type="text" placeholder="phone" onChange={handlePhone} />
-            <input type="text" placeholder = "description" onChange={handleDescription} />
+            <input type="text" placeholder="first name" onChange={handleFirstName} value={tempInquiry.first_name} />
+            <input type="text" placeholder="last name" onChange={handleLastName} value={tempInquiry.last_name}/>
+            <input type="text" placeholder="email" onChange={handleEmail} value={tempInquiry.email}/>
+            <input type="text" placeholder="phone" onChange={handlePhone} value={tempInquiry.phone}/>
+            <input type="text" placeholder = "description" onChange={handleDescription} value={tempInquiry.description}/>
             <button onClick={ ()=>addInquiry( tempInquiry ) }>Send inquiry</button>
         </>
     )
