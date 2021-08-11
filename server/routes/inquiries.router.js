8d9edb9c94f2025b30e598a3977c40cca29f4d58
Pat Anderson <patrick.b.anderson@gmail.com>
2021-08-11 13:49:02 -0500

    GET call working for inquiries

--- server/routes/inquiries.router.js
+++ server/routes/inquiries.router.js
@@ -5,12 +5,23 @@ const router = express.Router();
 //routes go here
 //POST for sending new inquiry data
 router.post( '/', ( req, res )=>{
-    console.log( 'in POST with:', req.body );
+    console.log( 'in inquiries POST with:', req.body );
     queryText = 'INSERT INTO "inquiries" ("first_name", "last_name", "email", "phone", "description") VALUES ($1, $2, $3, $4, $5);';
     pool.query( queryText, [ req.body.first_name, req.body.last_name, req.body.email, req.body.phone, req.body.description ]).then( ( results )=>{
         res.sendStatus( 200 );
     }).catch( ( err ) =>{
         console.log( 'error in inquiry POST:', err );
+        res.sendStatus( 500 );
+    })
+})
+//GET call for bringing down new inquiries
+router.get( '/', ( req, res )=>{
+    console.log( 'in inquiries GET' );
+    queryText = 'SELECT * FROM "inquiries" ORDER BY "date" DESC;';
+    pool.query( queryText ).then( ( results )=>{
+        res.send( results.rows );
+        res.sendStatus( 201 );
+    }).catch( ( err )=>{
         res.sendStatus( 500 );
     })
 })
